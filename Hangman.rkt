3fa2df817cf95fd30d7d9999a9d5db671208bf3a
Rúben Costa <rubencosta@MacBook-Pro-de-Ruben-Costa.local>
2018-03-18 19:55:05 +0100

    added author info

--- Hangman.rkt
+++ Hangman.rkt
@@ -1,6 +1,9 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname Hangman) (read-case-sensitive #t) (teachpacks ((lib "draw.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "draw.rkt" "teachpack" "htdp")) #f)))
+;; Author: Rúben Costa
+;; rubencosta@live.com
+
 ;; Exercise 6.7 in the book:
 ;; " HOW TO DESIGN PROGRAMS - An Introduction to Programming and Computing "
 
