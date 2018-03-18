db16a37eeb711e53a5fad0d678d373433b4f2cf6
Rúben Costa <rubencosta@MacBook-Pro-de-Ruben-Costa.local>
2018-03-18 19:54:05 +0100

    added author info

--- movingcirclesrectangle1.rkt
+++ movingcirclesrectangle1.rkt
@@ -1,6 +1,9 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname movingcirclesrectangle1) (read-case-sensitive #t) (teachpacks ((lib "draw.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "draw.rkt" "teachpack" "htdp")) #f)))
+;; Author: Rúben Costa
+;; rubencosta@live.com
+
 ;; Exercise 6.6 in the book:
 ;; " HOW TO DESIGN PROGRAMS - An Introduction to Programming and Computing "
 
